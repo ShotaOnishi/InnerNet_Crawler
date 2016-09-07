b0c24e94d5d0c856c7d5543e63ae675ccce3ae3b
Yuta Ide <yIde4949@gmail.com>
2016-09-07 23:48:55 +0900

    [WIP] prolog

--- prolog/knowledge.pl
+++ prolog/knowledge.pl
@@ -6,5 +6,3 @@ language(prolog).
 library(javascript, angularJS).
 library(javascript, reactJS).
 library(python, bottle).
-write_lang :- language(X), write(X), nl, fail.
-write_library(Y) :- library(Y, X), write(X), nl, fail.
