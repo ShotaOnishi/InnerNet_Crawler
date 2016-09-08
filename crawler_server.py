ba0d121051782b9c2388507ee8bb1e9a227c9b77
Yuta Ide <yIde4949@gmail.com>
2016-09-08 23:47:35 +0900

    [update] to add query.txt

--- crawler_server.py
+++ crawler_server.py
@@ -12,6 +12,7 @@ kill
 
 from bottle import route, run, request, response
 from get_pages import get_pages_from_google
+import subprocess
 
 # curl -X GET "http://127.0.0.1:8080/get_pages?query=react+Redux"
 @route('/get_pages')
@@ -19,11 +20,20 @@ def get_pages():
     print("get_pagesへアクセスされました. ")
     response.set_header('Access-Control-Allow-Origin','*')
     query = request.query.get('query')
+    for i in query:
+        cmd = "swipl -q -f ./prolog/main.pl -t main '%s' > query.txt"(i)
+        subprocess.call( cmd, shell=True)
+        f = open('./query.txt', 'r')
+        str_list = f.readlines()
+        str_list = map(str.strip,str_list) #改行削除
+        str_set = set(str_list)
     get_pages_from_google(query)
     return 'got page'
 
 @route('/test')
 def test():
-    return 'test'
+    cmd = "swipl -q -f ./prolog/main.pl -t main 'javascript' > query.txt"
+    subprocess.call( cmd, shell=True)
+    return "test"
 
 run(host='localhost', port=8080, debug=True, reloader=True)
--- get_pages.py
+++ get_pages.py
@@ -13,13 +13,13 @@ def get_pages_from_google(query):
     query_list = query.split()
     url_links = []
     browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
-    print("PhantomJS生成 ")
+    print("[GET PAGES]PhantomJS生成 ")
     browser.get(GOOGLE_URL)
-    print("googleに飛ぶ")
+    print("[GET PAGES]googleに飛ぶ")
     search_input = browser.find_element_by_name('q') #inputフォームを見つける
     search_input.send_keys(query) # そのフォームにqueryメッセージをぶち込む
     search_input.submit() # そのqueryを送る
-    print("googleで検索")
+    print("[GET PAGES]googleで検索")
     time.sleep(1) # 1秒待つ (待たないと, 結果が全部表示される前に次のページに移るため. )
     titles = browser.find_elements_by_css_selector('h3.r') # 検索結果のタイトルを取得する. (webelementのリストで帰ってくる. )
     # 来訪すべきURLのリストを一つ一つ訪れる
@@ -27,20 +27,20 @@ def get_pages_from_google(query):
         i = i.find_elements_by_css_selector('a') #webelementからアンカータグのwebelementを取り出す. (リスト形式で返ってくる)
         url_link = i[0].get_attribute('href') # アンカータグのwebelementに含まれているhrefのリンク先を取得
         url_links.append(url_link) # url_linksにurl_linkを追加していく
-    print("urlリスト完成")
+    print("[GET PAGES]urlリスト完成")
     for i in url_links:
         browser.get(i)
-        print("個別ページにアクセス")
+        print("[GET PAGES]個別ページにアクセス")
         title = browser.title
-        print("title取得")
+        print("[GET PAGES]title取得")
         url = i
-        print("現在のurl")
+        print("[GET PAGES]現在のurl")
         domain = tldextract.extract(url).domain
-        print("urlのdomain")
+        print("[GET PAGES]urlのdomain")
         file_name = os.path.join(os.path.dirname(os.path.abspath("__file__")), 'saved_images', str(uuid.uuid4())+'page.png')
-        print("いろいろなプロパティ取得")
+        print("[GET PAGES]いろいろなプロパティ取得")
         browser.save_screenshot(file_name)
-        print("スクショ撮影")
+        print("[GET PAGES]スクショ撮影")
         save_pages(tags=query_list, image=file_name, title=title, domain=domain, url=url)
         time.sleep(1)
 
