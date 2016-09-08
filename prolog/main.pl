485f7628645f6118955b95401f828924dd656990
Yuta Ide <yIde4949@gmail.com>
2016-09-08 23:26:12 +0900

    [upgrate] print tag to txt

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,8 +1,8 @@
 :- initialization main.
 main :-
-  ['./control.pl'],
-  ['./knowledge.pl'],
+  ['./prolog/control.pl'],
+  ['./prolog/knowledge.pl'],
   current_prolog_flag(argv, Argv),
   nth0(0, Argv, Argument0),
   write_library(Argument0),
-  halt(0).
+  halt.
